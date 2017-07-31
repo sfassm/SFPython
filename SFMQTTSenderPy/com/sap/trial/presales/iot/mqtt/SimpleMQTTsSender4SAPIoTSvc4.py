48b4359865b50a75439d754f44e76b52918a4112
kelte <g1Th0bus3r>
2017-07-31 18:23:13 +0200

    MQTT Sender successfully tested with IoTSvc 4.0 !!! Client-ID MUST be provided in Client() class, DEVICE_ID and LOG_NODE_ADDR (of Sensor) ARE case-sensitive!

--- SFMQTTSenderPy/com/sap/trial/presales/iot/mqtt/SimpleMQTTsSender4SAPIoTSvc4.py
+++ SFMQTTSenderPy/com/sap/trial/presales/iot/mqtt/SimpleMQTTsSender4SAPIoTSvc4.py
@@ -6,29 +6,41 @@ Created on 31 Jul 2017
 import paho.mqtt.client as paho
 import time
 import json
+import random
 
 # capture receipt when successfully published
 def on_publish(client, userdata, msg_id):
     print("on_publish called, msg_id: " + str(msg_id) + " successfully published.\n")
-    
+
+# Addresses and IDs are case-sensitive!!!    
 DEVICE_ID = "10:12:68:FA:01"                        # Device physical address / Unique Address, e.g. MAC address '5e0a270d79f9389b' w/o ':' separators
-LOG_NODE_ADDR = "SF_Local_PAHO_UIClient_Sensor_1"   # Sensor address / physical address: "60c8faf8d6560b7c"
+#LOG_NODE_ADDR = "00:00:00:02" #"SF_Local_PAHO_UIClient_Sensor_1"   # Sensor address / physical address: "60c8faf8d6560b7c"
+LOG_NODE_ADDR = "SF_Local_PAHO_UIClient_Sensor_1"
 PUB_TOPIC = "measures/" + DEVICE_ID                 # MQTT pub topic
 
-i = 1
-timeIntervall = 15
+timeIntervall = 5
 temperature = 20
 humidity = 40
 
-mqttclnt = paho.Client()
+mqttclnt = paho.Client(client_id=DEVICE_ID)
 mqttclnt.on_publish = on_publish
 #mqttclnt.username_pw_set("root#0", "KYTAcbyNY5qlsmh")
 mqttclnt.tls_set(certfile="certificate.pem", keyfile="plainkey.pem") # downloaded from IoTServices for this device as *.ks file, needs to be converted into *.pem files
 mqttclnt.connect("iotae-beta03.eu10.cp.iot.sap", 8883)
-#data = json.dumps({"profileId":4,"measureIds":[510],"values": ['{}'.format(temperature), '{}'.format(humidity)],"logNodeAddr":logNodeAddrStr})
-data = json.dumps({"measureIds":[510],"values": ['{}'.format(temperature), '{}'.format(humidity)],"logNodeAddr":LOG_NODE_ADDR})
-print("Publishing message under topic=" + PUB_TOPIC + ", JSON body: " + data)
+
+while True:
+    try:
+        # Create some random values
+        humidity = random.randint(0, 100)
+        temperature = random.randint(10, 30)
+        #data = json.dumps({"profileId":4,"measureIds":[510],"values": ['{}'.format(temperature), '{}'.format(humidity)],"logNodeAddr":logNodeAddrStr})
+        data = json.dumps({"profileId":4,"measureIds":[510],"values": ['{}'.format(temperature), '{}'.format(humidity)],"logNodeAddr":LOG_NODE_ADDR})
+        print("Publishing message under topic=" + PUB_TOPIC + ", JSON body: " + data)
+        # Publish data:
+        (rc, msg_id) = mqttclnt.publish(PUB_TOPIC, data)
+        # wait some time to read again sensor values
+        time.sleep(timeIntervall)
+    
+    except IOError:
+        print ("Error")
  
-for i in range(1, 3):
-    (rc, msg_id) = mqttclnt.publish(PUB_TOPIC, data)
-    time.sleep(timeIntervall)
